0666c29e83eac30eb51ce04098f2f7d8985d4290
sibinski <jevgeni.shibinski@gmail.com>
2024-05-28 04:15:41 -0400

    Back-end Integration for Login

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -53,4 +53,48 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        if(theUser) 
+        {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if(!result)
+            {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong password'});
+            }
+        
+        // Task 5: Fetch user details from a database
+        const userName = theUser.firstName;
+        const userEmail = theUser.email;
+        // Task 6: Create JWT authentication if passwords match with user._id as payload
+        let payload = {
+            user: {
+                id: theUser._id.toString(),
+            },
+        };
+        jwt.sign(user._id, JWT_SECRET);
+        logger.info('User logged in successfully');
+        return res.status(200).json({authtoken, userName, userEmail });
+        // Task 7: Send appropriate message if the user is not found
+    }
+    else
+        {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (e) {
+        logger.error(e);
+         return res.status(500).send('Internal server error');
+    }
+});
+
 module.exports = router;
