1cdfa54d6edbbbb34863b41b0befe75c61397545
sibinski <jevgeni.shibinski@gmail.com>
2024-06-08 15:49:00 -0400

    updated

--- sentiment/index.js
+++ sentiment/index.js
@@ -14,38 +14,38 @@ app.use(expressPino)
 // Task 3: create the POST /sentiment analysis
 app.post('/sentiment', async (req, res) => {
   // Task 4: extract the sentence parameter
-  const { sentence } = req.query;
+  const { sentence } = req.query
   if (!sentence) {
-      logger.error('No sentence provided');
-      return res.status(400).json({ error: 'No sentence provided' });
+      logger.error('No sentence provided')
+      return res.status(400).json({ error: 'No sentence provided' })
   }
   // Initialize the sentiment analyzer with the Natural's PorterStemmer and "English" language
-  const Analyzer = natural.SentimentAnalyzer;
-  const stemmer = natural.PorterStemmer;
-  const analyzer = new Analyzer("English", stemmer, "afinn");
+  const Analyzer = natural.SentimentAnalyzer
+  const stemmer = natural.PorterStemmer
+  const analyzer = new Analyzer("English", stemmer, "afinn")
   // Perform sentiment analysis
   try {
-      const analysisResult = analyzer.getSentiment(sentence.split(' '));
-      let sentiment = "neutral";
+      const analysisResult = analyzer.getSentiment(sentence.split(' '))
+      let sentiment = "neutral"
         // Task 5: set sentiment to negative or positive based on score rules
         if(analysisResult < 0) 
         {
-          sentiment = "negative";
+          sentiment = "negative"
         }
         else (analysisResult > 0.33)
         {
-          sentiment = "positive";
+          sentiment = "positive"
         }
       // Logging the result
-      logger.info(`Sentiment analysis result: ${analysisResult}`);
+      logger.info(`Sentiment analysis result: ${analysisResult}`)
       // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
       res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
     } catch (error) {
-      logger.error(`Error performing sentiment analysis: ${error}`);
+      logger.error(`Error performing sentiment analysis: ${error}`)
       // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-      res.status(500).json(message, 'Error performing sentiment analysis');
+      res.status(500).json(message, 'Error performing sentiment analysis')
     }
 })
 app.listen(port, () => {
-    logger.info(`Server running on port ${port}`);
+    logger.info(`Server running on port ${port}`)
 })
