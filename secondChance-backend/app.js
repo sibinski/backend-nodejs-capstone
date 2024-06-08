e519611f4fb53fbba55fe5cab14948bfce7462e2
sibinski <jevgeni.shibinski@gmail.com>
2024-06-08 12:52:16 -0400

    update or fixed lab

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -45,11 +45,11 @@ app.use('/api/secondchance/search', searchRoutes)
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error')
+  console.error(err)
+  res.status(500).send('Internal Server Error')
 })
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.send('Inside the server')
 })
 
