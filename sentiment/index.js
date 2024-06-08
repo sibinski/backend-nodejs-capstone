f3217ddddc9632aa17036ce3a6b0fe94cc432e22
sibinski <jevgeni.shibinski@gmail.com>
2024-06-08 16:26:49 -0400

    updated

--- sentiment/index.js
+++ sentiment/index.js
@@ -31,9 +31,9 @@ app.post('/sentiment', async (req, res) => {
     if (analysisResult < 0) {
       sentiment = 'negative' // Changed double quotes to single quotes
     } else if (analysisResult > 0.33) // Corrected the else statement
-      {
-      sentiment = 'positive' // Changed double quotes to single quotes
-    }
+    {
+      sentiment = 'positive'; // Changed double quotes to single quotes
+      }
     // Logging the result
     logger.info(`Sentiment analysis result: ${analysisResult}`)
     // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
